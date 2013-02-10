1874a97e3d354fc21dc8b4812c45946709d7dbc7
Christoph Schueler <cpu12.gems@googlemail.com>
2013-02-10 23:20:29 +0100

    Templates adapted to Mako.

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from distutils.core import setup,Extension
 from setuptools import find_packages
 from glob import glob
 
-#print find_packages('pySART')
 
 setup(
-    name='pySART.kosek',
-    version='0.9.2rc-1',
-    description="'Konnex-Operationg System based on the OSEK/VDX-Standard'-Distribution",
-    author='Christoph Schueler',
-    author_email='cpu12.gems@googlemail.com',
-    url='http://www.github.com/Christoph2/k-os',
-    packages=['pySART.kosek', 'pySART.kosek.ApplicationDefinition', 'pySART.kosek.ImplementationDefinition'],
-    namespace_packages = ['pySART'],
+    name = 'kosek',	# pySART.kosek
+    version = '0.9.2rc-1',
+    description = "'Konnex-Operationg System based on the OSEK/VDX-Standard'-Distribution",
+    author = 'Christoph Schueler',
+    author_email = 'cpu12.gems@googlemail.com',
+    url = 'https://www.github.com/Christoph2/k-os',
+    packages = ['kosek', 'kosek.ApplicationDefinition', 'kosek.ImplementationDefinition'],
+    #namespace_packages = ['pySART'],
     data_files = [
 	    #('kosek/config/oil', glob('config/oil/*.oil')),	
 	    ('kosek/config/templates', glob('config/templates/*.tmpl')),
     ],
+    install_requires = ['mako', ],
     entry_points = {
 	'console_scripts': [
 		'kosgen = kosek.kosgen:main',
