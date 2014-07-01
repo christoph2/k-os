cf0bbfa84709a2e98302b995805f455520009994
Christoph Schueler <cpu12.gems@googlemail.com>
2014-07-01 08:48:30 +0200

    [Port_Asm.asm] Add 'OsPort_TaskStackInit'.

--- port/cpu12/codewarrior/Port_Asm.asm
+++ port/cpu12/codewarrior/Port_Asm.asm
@@ -1,7 +1,7 @@
 ;
 ;  k_os (Konnex Operating-System based on the OSEK/VDX-Standard).
 ;
-;  (C) 2007-2010 by Christoph Schueler <chris@konnex-tools.de,
+;  (C) 2007-2013 by Christoph Schueler <chris@konnex-tools.de,
 ;                                      cpu12.gems@googlemail.com>
 ;
 ;  All Rights Reserved
@@ -17,24 +17,25 @@
 ;
 ;  You should have received a copy of the GNU General Public License
 ;  along with this program; if not, write to the Free Software
-;  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+;  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ;
 ;  s. FLOSS-EXCEPTION.txt
 ;
- 
+
   PUBLIC CPU_INTERRUPTS_DISABLED
-  PUBLIC Utl_SetJump  
+  PUBLIC Utl_SetJump
   PUBLIC Utl_LongJump
   PUBLIC OsMLQ_GetLowestBitNumber
   PUBLIC Utl_Divrem
   PUBLIC Mem_GetStackPointer
   PUBLIC Mem_GetFreePointer
-  
+  PUBLIC  OsPort_TaskStackInit
+
   XREF  OsCurrentTCB,__OS_TOS_ISRStack
 
 MY_ROM: SECTION
 
-ReversedLog2Tab:   
+ReversedLog2Tab:
     dc.b    $00,$08,$07,$00,$06,$00,$00,$00,$05,$00,$00,$00,$00,$00,$00,$00
     dc.b    $04,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
     dc.b    $03,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
@@ -49,6 +50,9 @@ ReversedLog2Tab:
 ;.text
 
 
+RETURN_FROM_SUBROUTINE:  MACRO
+  rtc
+  ENDM
 
 CPU_INTERRUPTS_DISABLED:
     tfr     ccr,b
@@ -56,10 +60,10 @@ CPU_INTERRUPTS_DISABLED:
     bne     __id_l10
     clrb
     bra     __id_exit
-__id_l10:    
-    ldab    #$01    
-__id_exit:    
-    rts
+__id_l10:
+    ldab    #$01
+__id_exit:
+    RETURN_FROM_SUBROUTINE
 
 
 Utl_SetJump:
@@ -68,7 +72,7 @@ Utl_SetJump:
     sts     0,y
     ldd     #$0000
     rts
-    
+
 Utl_LongJump:
 ; 2,sp -> Pointer to Context.
 ; D -> Value.
@@ -76,10 +80,10 @@ Utl_LongJump:
     lds     2,y ; SP
     ldx     0,y ; PC
     tbne    d,__ljexit
-    inx 
-__ljexit:    
+    inx
+__ljexit:
     jmp     [0,x]
-    rts
+    RETURN_FROM_SUBROUTINE
 
 
 OsMLQ_GetLowestBitNumber:
@@ -94,7 +98,26 @@ L10:
     nop                         ; 1
 L20:
     clra                        ; 1
-    rts                         ; 5
+    RETURN_FROM_SUBROUTINE
+
+OsPort_TaskStackInit:
+; TODO: Calling conventions!!!
+;   D -> StackPointer
+;   X   -> TaskFunc
+    pshy
+    tfr     d,  y
+    ldx     0,  x
+    stx     1, -y
+
+;    movb    #$c0, 1 ,-y
+    leay     -6, y
+    movw     #$00c0, 2, -y
+
+    tfr     y,  d
+    puly
+    RETURN_FROM_SUBROUTINE
+
+
 
 ;
 ;   void Utl_Divrem(uint16 dividend,uint16 rhs,Utl_DivremType *res);
@@ -102,27 +125,39 @@ L20:
 Utl_Divrem:
 ;
 ;
-;   4,SP	-> dividend
-;   2,SP	-> divisor
-;   D	 	-> res
+;   4,SP        -> dividend
+;   2,SP        -> divisor
+;   D           -> res
     pshx
-    tfr		d,y
-    ldd		4,sp
-    ldx		2,sp
+    tfr         d,y
+    ldd         4,sp
+    ldx         2,sp
     idiv
-    std		2,y+
-    stx		0,y
+    std         2,y+
+    stx         0,y
     pulx
-    rts
+    RETURN_FROM_SUBROUTINE
 
 Mem_GetStackPointer:
     tfr     sp,d
-    rts
+    RETURN_FROM_SUBROUTINE
+
+Fhokker dc.b 1
+
+Spl_Get:
+  btas        Fhokker, #1
+  rts
+
+Spl_TryGet:
+  rts
+
+Spl_Release:
+  rts
 
 
 Mem_GetFreePointer:
 
-    rts
+    RETURN_FROM_SUBROUTINE
 
     END
 
