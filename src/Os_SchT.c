f4537c964b81734fc5f202262bc17830d4d360ff
Christoph Schueler <cpu12.gems@googlemail.com>
2026-03-03 06:13:34 +0200

    STM32 stuff

---
+++ src/Os_SchT.c
@@ -0,0 +1,2 @@
+/* Stub for Schedule Table module to satisfy build configuration when not provided. */
+/* Intentionally empty. */
---
+++ tools/oilcpp/src/Version.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string_view>
+
+namespace kos::oilcpp {
+
+inline constexpr std::string_view kVersion = "0.1.0-pre";
+
+}  // namespace kos::oilcpp
